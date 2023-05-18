bc353a417491377a064c32856f0ea5edc52e6bb1
vindosVp <vadiminmail@gmail.com>
2023-05-19 01:28:57 +0300

    db + users table

---
+++ internal/app/store/store.go
@@ -0,0 +1,48 @@
+package store
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+type Store struct {
+	config         *Config
+	db             *sql.DB
+	UserRepository *UserRepository
+}
+
+func New(config *Config) *Store {
+	return &Store{
+		config: config,
+	}
+}
+
+func (s *Store) User() *UserRepository {
+	if s.UserRepository != nil {
+		return s.UserRepository
+	}
+	s.UserRepository = &UserRepository{
+		store: s,
+	}
+	return s.UserRepository
+}
+
+func (s *Store) Open() error {
+	db, err := sql.Open("postgres", s.config.DatabaseURL)
+	if err != nil {
+		return err
+	}
+	if err := db.Ping(); err != nil {
+		return err
+	}
+	s.db = db
+	return nil
+}
+
+func (s *Store) Close() error {
+	err := s.db.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
