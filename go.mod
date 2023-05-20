a82446f2a49c181ebdf747575aeee85c33a40b07
vindosVp <vadiminmail@gmail.com>
2023-05-20 17:09:59 +0300

    pwd

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/sirupsen/logrus v1.9.2 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/stretchr/testify v1.8.2 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/app/model/users.go
+++ internal/app/model/users.go
@@ -1,9 +1,27 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	UUID              uuid.UUID
 	Email             string
+	Password          string
 	EncryptedPassword string
+}
+
+func (u *User) BeforeCreate() error {
+	return nil
+}
+
+func encryptStr(str string) (string, error) {
+	encString, err := bcrypt.GenerateFromPassword([]byte(str), bcrypt.MinCost)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(encString), nil
 }
